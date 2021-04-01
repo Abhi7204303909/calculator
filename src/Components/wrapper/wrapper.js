3c99ba6185cda3bd4e198b08c53ae2bf5b562cee
Abhi7204303909 <abhishekmkatti@gmail.com>
2021-04-02 00:13:14 +0530

    'commited''

--- src/Components/wrapper/wrapper.js
+++ src/Components/wrapper/wrapper.js
@@ -6,38 +6,122 @@ class Wrapper extends Component{
     state={
        
        
-        str:''
+        str:'',
+        res:null,
+        
     }
+ 
     foo(val){
-        let arr=val.split(' ')
-        if(val==='+'){}
-        if(val==='-'){}
-        if(val==='*'){}
-        if(val==='/'){}
+        let arr=this.state.str.split(' ');
+        // console.log(arr);
+       
+        if(arr[1]==='+'){
+            let sum=0;
+         
+            sum=Number(arr[0])+Number(arr[2]);
+            for(let i=3;i<arr.length;i++){
+                    if(arr[i]==='+')continue;
+                    
+                    sum+=Number(arr[i]);
+            }
+            if(val==='='){
+                this.setState({str:''+sum});
+            }
+         
+               
+            
+        }
+        if(arr[1]==='-'){
+            let diff=arr[0];
+         
+            diff-=Number(arr[2]);
+            for(let i=3;i<arr.length;i++){
+                    if(arr[i]==='+')continue;
+                    
+                    diff-=Number(arr[i]);
+            }
+            if(val==='='){
+                this.setState({str:''+diff});
+            }
+         
+               
+        }
+        if(arr[1]==='*'){
+            let mul=arr[0];
+         
+            mul*=Number(arr[2]);
+            for(let i=3;i<arr.length;i++){
+                    if(arr[i]==='+')continue;
+                    
+                    mul*=Number(arr[i]);
+            }
+            if(val==='='){
+                this.setState({str:''+mul});
+            }
+         
+        }
+        if(arr[1]==='/'){
+            let div=arr[0];
+         
+            div/=Number(arr[2]);
+            for(let i=3;i<arr.length;i++){
+                    if(arr[i]==='+')continue;
+                    
+                    div/=Number(arr[i]);
+            }
+            if(val==='='){
+                this.setState({str:''+div});
+            }
+         
+        }
+        if(arr[1]==='%'){
+            let per;
+            per=Number(arr[0])/100;
+            if(val==='='){
+                this.setState({str:''+per});
+            }
+
+        }
+       
     }
     clickedHandler=(event)=>{
-        console.log(event);
+        // console.log(event);
+        event.preventDefault();
         let val=event.target.textContent;
         let prevStr;
+        if(val==='AC'){
+            this.setState({str:''});
+            return;
+        }
+        if(val==='C'){
+            let ar=this.state.str.split(' ');
+            ar.pop();
+            this.setState({str:ar.join(' ')});
+            return;
+        }
         if(this.state.str!==''){
             prevStr=this.state.str[this.state.str.length-1];
-            console.log(prevStr);
+            // console.log(prevStr);
         }
         // console.log(prevStr);
-        if(isFinite(parseInt(val)) && prevStr!=='+'){
-            this.setState({str:this.state.str+val})
+        if(isFinite(parseInt(val))||val==='.' && (prevStr!=='+' || prevStr!=='-' || prevStr!=='=' || prevStr!=='/')){
+            this.setState((prevState)=>{return{str:prevState.str+val}})
+            this.foo(val);
         }
         else{
-            this.setState({str:this.state.str+' '+val})
+            this.setState((prevState)=>{return {str:prevState.str+' '+val+' '}})
+            this.foo(val);
         }
+           
+        
       
-   this.foo(this.state.str);
+ 
  
 
 
     }
     render(){
-
+//    console.log('inside render',this.state);
         return (
             <Aux>
                 <Input value={this.state.str}/>
